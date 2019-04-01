6f368833f1b37523297de99ff87b6371dfa13b07
verpixelt <mail@kevinlorenz.com>
2019-04-01 22:11:51 +0200

    initial commit

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -7,37 +7,34 @@ import { graphql } from 'gatsby'
 
 function BlogPost(props) {
 
-    const post = props.data.markdownRemark;
-    const url = props.data.site.siteMetadata.siteUrl
-    const { title, description } = post.frontmatter;
-    const thumbnail =
-        post.frontmatter.image &&
-        post.frontmatter.image.childImageSharp.resize.src
+  const post = props.data.markdownRemark;
+  const url = props.data.site.siteMetadata.siteUrl
+  const { title, description } = post.frontmatter;
+  const thumbnail =
+    post.frontmatter.image &&
+    post.frontmatter.image.childImageSharp.resize.src
 
-    return (
-        <Layout>
-            <Metatags
-                title={title}
-                description={description}
-                thumbnail={thumbnail ? url + thumbnail : url + Icon}
-                url={url}
-                pathname={props.location.pathname}
-            />
-            <div>
-                <h1>{title}</h1>
-                {thumbnail && <Img fluid={post.frontmatter.image.childImageSharp.fluid} />}
-                <div dangerouslySetInnerHTML={{ __html: post.html }} />
-            </div>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <Metatags
+        title={title}
+        description={description}
+        thumbnail={thumbnail ? url + thumbnail : url + Icon}
+        url={url}
+        pathname={props.location.pathname}
+      />
+      <div>
+        <h1>{title}</h1>
+        {thumbnail && <Img fluid={post.frontmatter.image.childImageSharp.fluid} />}
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </Layout>
+  )
 }
-
 
 export default BlogPost
 
-
 export const query = graphql`
-
  query PostQuery($slug: String!) {
      markdownRemark(fields: { slug: { eq: $slug } }) {
        html
