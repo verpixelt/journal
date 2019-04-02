95625cbde13f1c4bd7848c21b751f2e0b0266db7
Kevin Lorenz <kl@idagio.com>
2019-04-02 13:26:17 +0200

    Change typeface, delete font files, change layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,45 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
+import styled from 'styled-components';
+
+const ListLink = styled(Link)`
+  display: inline-block;
+`
+
+const List = styled.ul`
+  padding-left: 0;
+  list-style: none;
+  margin-top: 0;
+  margin-bottom: 0;
+`
+
+const ListEl = styled.li`
+  &:not(:last-child) { margin-bottom: 6rem; }
+`
+
+const Date = styled.time`
+  font-family: var(--sansSerifFS);
+  display: block;
+  margin-bottom: .5rem;
+  font-weight: 900;
+`
 
 const IndexPage = (props) => {
   const postList = props.data.allMarkdownRemark;
   return (
     <Layout>
-      {postList.edges.map(({ node }, i) => (
-        <Link to={node.fields.slug} key={i} className="link" >
-          <h1>{node.frontmatter.title}</h1>
-          <span>{node.frontmatter.date}</span>
-          <p>{node.excerpt}</p>
-        </Link>
-      ))}
+      <List>
+        {postList.edges.map(({ node }, i) => (
+          <ListEl key={i}>
+            <ListLink to={node.fields.slug} className="link" >
+              <h1>{node.frontmatter.title}</h1>
+              <Date>{node.frontmatter.date}</Date>
+              <span>{node.excerpt}</span>
+            </ListLink>
+          </ListEl>
+        ))}
+      </List>
     </Layout>
   )
 }
